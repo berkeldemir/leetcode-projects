d992a20e2b0b44bec693f61f204ce9c86fb04ea0
bermuvia <88797969+bermuvia@users.noreply.github.com>
2024-08-24 00:37:15 +0300

    9th Polindrome Number / 2nd try / fail / Wrong answer (how... lemme think)

---
+++ 9-polindrome-number/2f-wronganswer.c
@@ -0,0 +1,32 @@
+#include <stdbool.h>
+unsigned int timesten(unsigned int m)
+{
+	unsigned int toreturn = 1;
+	while(m > 0)
+	{
+		toreturn *= 10;
+		m--;
+	}
+	return (toreturn);
+}
+
+bool isPalindrome(int x)
+{
+	if (x < 0) return false;
+	int y = x;
+	int n = 0;
+	while (y != 0)
+	{
+		y /= 10;
+		n++;
+	}
+	int m = n;
+
+	while ((m / 2) > 0)
+	{
+		if ((x % timesten(m) / timesten(m-1)) != (x % timesten(n - m + 1) / timesten (n - m)))
+			return false;
+		m--;
+	}
+	return true;
+}
